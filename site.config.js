3222f71717587865b952405b602d76a3145009c2
youngkeol <41726750+youngkeol@users.noreply.github.com>
2023-02-13 16:58:32 +0900

    [2023.02.13]  설정 수정
    설정 수정

--- site.config.js
+++ site.config.js
@@ -19,12 +19,12 @@ const CONFIG = {
   // blog setting (required)
   blog: {
     title: 'gory-log',
-    description: 'welcome to morethan-log!',
+    description: 'welcome to gory-log!',
     theme: 'light' // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: 'https://morethan-log.vercel.app',
+  link: 'https://gory-log.vercel.app',
   since: 2022, // If leave this empty, current year will be used.
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: 'https://og-image-korean.vercel.app', // The link to generate OG image, don't end with a slash
@@ -52,7 +52,7 @@ const CONFIG = {
     }
   },
   utterances: {
-    enable: true,
+    enable: false,
     config: {
       repo: 'morethanmin/morethan-log',
       'issue-term': 'og:title',
