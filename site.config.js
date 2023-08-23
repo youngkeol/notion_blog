93009ddb2fcd6621849fd5333e6cf8f555c26209
youngkeol <hyk1272@gmail.com>
2023-08-23 09:43:31 +0900

    [2923.08.23] 재업데이트
    재업데이트

--- site.config.js
+++ site.config.js
@@ -28,9 +28,6 @@ const CONFIG = {
   since: 2022, // If leave this empty, current year will be used.
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: 'https://og-image-korean.vercel.app', // The link to generate OG image, don't end with a slash
-  seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
-  },
 
   // notion configuration (required)
   notionConfig: {
@@ -52,13 +49,21 @@ const CONFIG = {
     }
   },
   utterances: {
+    enable: true,
+    config: {
+      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
+      "issue-term": "og:title",
+      label: "💬 Utterances",
+    },
+  },
+  cusdis: {
     enable: false,
     config: {
-      repo: 'morethanmin/morethan-log',
-      'issue-term': 'og:title',
-      label: '💬 Utterances',
-    }
+      host: "https://cusdis.com",
+      appid: "", // Embed Code -> data-app-id value
+    },
   },
   isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+  revalidateTime: 21600 * 7, // revalidate time for [slug], index
 }
-module.exports = CONFIG
+module.exports = { CONFIG }
