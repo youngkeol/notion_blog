fa294cd9e5588d9c63b648108368b8d6335aa2bb
youngkeol <hyk1272@gmail.com>
2023-08-23 10:03:14 +0900

    [2023.08.23] 재업로드
    재업로드

--- site.config.js
+++ site.config.js
@@ -1,51 +1,50 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: 'gory',
-    image: '/avatar.svg',  // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: 'developer',
-    bio: 'Hello 👋',
-    email: 'younggory.work@gmail.com',
-    linkedin: '',
-    github: 'youngkeol',
-    instagram: '',
+    name: "morethanmin",
+    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    role: "frontend developer",
+    bio: "I develop everything using node.",
+    email: "morethanmin.dev@gmail.com",
+    linkedin: "morethanmin",
+    github: "morethanmin",
+    instagram: "",
   },
   projects: [
     {
-      name: `gory-log`,
-      href: 'https://github.com/youngkeol'
-    }
+      name: `morethan-log`,
+      href: "https://github.com/morethanmin/morethan-log",
+    },
   ],
   // blog setting (required)
   blog: {
-    title: 'gory-log',
-    description: 'welcome to gory-log!',
+    title: "morethan-log",
+    description: "welcome to morethan-log!",
   },
 
   // CONFIG configration (required)
-  link: 'https://gory-log.vercel.app',
+  link: "https://morethan-log.vercel.app",
   since: 2022, // If leave this empty, current year will be used.
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: 'https://og-image-korean.vercel.app', // The link to generate OG image, don't end with a slash
+  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
   notionConfig: {
     pageId: process.env.NOTION_PAGE_ID,
-    //pageId: '6894b003885841f980ef54789ce969bc',
   },
 
   // plugin configuration (optional)
   googleAnalytics: {
     enable: false,
     config: {
-      measurementId: process.env.GOOGLE_MEASUREMENT_ID || ''
-    }
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+    },
   },
   googleSearchConsole: {
     enable: false,
     config: {
-      siteVerification: process.env.GOOGLE_SITE_VERIFICATION || ''
-    }
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+    },
   },
   utterances: {
     enable: true,
@@ -62,7 +61,8 @@ const CONFIG = {
       appid: "", // Embed Code -> data-app-id value
     },
   },
-  isProd: process.env.VERCEL_ENV === 'production', // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
   revalidateTime: 21600 * 7, // revalidate time for [slug], index
 }
+
 module.exports = { CONFIG }
