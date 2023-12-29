cc087b25c2db2b3c32a95e303ea0d12fef9a08d3
youngkeol <hyk1272@gmail.com>
2023-12-29 10:46:37 +0900

    [2023.12.29] 계정 명 변경
    계정 명 변경

--- site.config.js
+++ site.config.js
@@ -1,69 +1,70 @@
 const CONFIG = {
-  // profile setting (required)
-  profile: {
-    name: "Gory",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "",
-    bio: "Hello 👋",
-    email: "younggory.work@gmail.com",
-    linkedin: "",
-    github: "youngkeol",
-    instagram: "",
-  },
-  projects: [
-    {
-      name: `gory-log`,
-      href: 'https://github.com/youngkeol'
+    // profile setting (required)
+    profile: {
+        name: "Gori",
+        image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+        role: "",
+        bio: "Hello 👋",
+        email: "younggory.work@gmail.com",
+        linkedin: "",
+        github: "youngkeol",
+        instagram: "",
     },
-  ],
-  // blog setting (required)
-  blog: {
-    title: "Gory",
-    description: "welcome to Gory!",
-  },
+    projects: [
+        {
+            name: `gory-log`,
+            href: "https://github.com/youngkeol",
+        },
+    ],
+    // blog setting (required)
+    blog: {
+        title: "Gori",
+        description: "welcome to Gori!",
+    },
 
-  // CONFIG configration (required)
-  link: "https://gory-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+    // CONFIG configration (required)
+    link: "https://gori-log.vercel.app",
+    since: 2022, // If leave this empty, current year will be used.
+    lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+    ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
-  // notion configuration (required)
-  notionConfig: {
-    pageId: process.env.NOTION_PAGE_ID,
-  },
+    // notion configuration (required)
+    notionConfig: {
+        pageId: process.env.NOTION_PAGE_ID,
+    },
 
-  // plugin configuration (optional)
-  googleAnalytics: {
-    enable: false,
-    config: {
-      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+    // plugin configuration (optional)
+    googleAnalytics: {
+        enable: false,
+        config: {
+            measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+        },
     },
-  },
-  googleSearchConsole: {
-    enable: false,
-    config: {
-      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+    googleSearchConsole: {
+        enable: false,
+        config: {
+            siteVerification:
+                process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+        },
     },
-  },
-  utterances: {
-    enable: false,
-    config: {
-      //repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
-      repo: 'morethanmin/mormin-log',
-      "issue-term": "og:title",
-      label: "💬 Utterances",
+    utterances: {
+        enable: false,
+        config: {
+            //repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO,
+            repo: "morethanmin/mormin-log",
+            "issue-term": "og:title",
+            label: "💬 Utterances",
+        },
     },
-  },
-  cusdis: {
-    enable: false,
-    config: {
-      host: "https://cusdis.com",
-      appid: "", // Embed Code -> data-app-id value
+    cusdis: {
+        enable: false,
+        config: {
+            host: "https://cusdis.com",
+            appid: "", // Embed Code -> data-app-id value
+        },
     },
-  },
-  isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-  revalidateTime: 21600 * 7, // revalidate time for [slug], index
-}
+    isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+    revalidateTime: 21600 * 7, // revalidate time for [slug], index
+};
 
-module.exports = { CONFIG }
+module.exports = { CONFIG };
