7e712bdc5324ade0b72049188aa0e221ba9519cf
youngkeol <hyk1272@gmail.com>
2024-01-22 13:18:11 +0900

    [2024.01.22] console 출력 확인용
    console 출력 확인용

--- src/hooks/useCategoriesQuery.ts
+++ src/hooks/useCategoriesQuery.ts
@@ -1,13 +1,16 @@
-import { DEFAULT_CATEGORY } from "src/constants"
-import usePostsQuery from "./usePostsQuery"
-import { getAllSelectItemsFromPosts } from "src/libs/utils/notion"
+import { DEFAULT_CATEGORY } from "src/constants";
+import usePostsQuery from "./usePostsQuery";
+import { getAllSelectItemsFromPosts } from "src/libs/utils/notion";
 
 export const useCategoriesQuery = () => {
-  const posts = usePostsQuery()
-  const categories = getAllSelectItemsFromPosts("category", posts)
+    const posts = usePostsQuery();
+    const categories = getAllSelectItemsFromPosts("category", posts);
 
-  return {
-    [DEFAULT_CATEGORY]: posts.length,
-    ...categories,
-  }
-}
+    console.log("AAA ");
+    console.log(categories);
+
+    return {
+        [DEFAULT_CATEGORY]: posts.length,
+        ...categories,
+    };
+};
