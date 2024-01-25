ddf94a98dd2baeb214524e8b80a7fba8101a23fc
youngkeol <hyk1272@gmail.com>
2024-01-25 15:32:54 +0900

    [2024.01.25] 카테고리 제거
     카테고리 제거

--- src/hooks/useCategoriesQuery.ts
+++ src/hooks/useCategoriesQuery.ts
@@ -5,10 +5,6 @@ import { getAllSelectItemsFromPosts } from "src/libs/utils/notion";
 export const useCategoriesQuery = () => {
     const posts = usePostsQuery();
     const categories = getAllSelectItemsFromPosts("category", posts);
-
-    console.log("AAA ");
-    console.log(categories);
-
     return {
         [DEFAULT_CATEGORY]: posts.length,
         ...categories,
--- src/routes/Feed/FeedHeader/CategorySelect.tsx
+++ src/routes/Feed/FeedHeader/CategorySelect.tsx
@@ -1,30 +1,34 @@
-import useDropdown from "src/hooks/useDropdown"
-import { useRouter } from "next/router"
-import React from "react"
-import { MdExpandMore } from "react-icons/md"
-import { DEFAULT_CATEGORY } from "src/constants"
-import styled from "@emotion/styled"
-import { useCategoriesQuery } from "src/hooks/useCategoriesQuery"
+import useDropdown from "src/hooks/useDropdown";
+import { useRouter } from "next/router";
+import React from "react";
+import { MdExpandMore } from "react-icons/md";
+import { DEFAULT_CATEGORY } from "src/constants";
+import styled from "@emotion/styled";
+import { useCategoriesQuery } from "src/hooks/useCategoriesQuery";
 
-type Props = {}
+type Props = {};
 
 const CategorySelect: React.FC<Props> = () => {
-  const router = useRouter()
-  const data = useCategoriesQuery()
-  const [dropdownRef, opened, handleOpen] = useDropdown()
+    const router = useRouter();
+    const data = useCategoriesQuery();
+    const [dropdownRef, opened, handleOpen] = useDropdown();
 
-  const currentCategory = `${router.query.category || ``}` || DEFAULT_CATEGORY
+    const currentCategory =
+        `${router.query.category || ``}` || DEFAULT_CATEGORY;
 
-  const handleOptionClick = (category: string) => {
-    router.push({
-      query: {
-        ...router.query,
-        category,
-      },
-    })
-  }
-  return (
-    <StyledWrapper>
+    const handleOptionClick = (category: string) => {
+        router.push({
+            query: {
+                ...router.query,
+                category,
+            },
+        });
+    };
+    return (
+        <StyledWrapper>
+            <div>&nbsp;</div>
+            {/*  수정
+          
       <div ref={dropdownRef} className="wrapper" onClick={handleOpen}>
         {currentCategory} Posts <MdExpandMore />
       </div>
@@ -40,48 +44,49 @@ const CategorySelect: React.FC<Props> = () => {
             </div>
           ))}
         </div>
-      )}
-    </StyledWrapper>
-  )
-}
+          )}
+           */}
+        </StyledWrapper>
+    );
+};
 
-export default CategorySelect
+export default CategorySelect;
 
 const StyledWrapper = styled.div`
-  position: relative;
-  > .wrapper {
-    display: flex;
-    margin-top: 0.5rem;
-    margin-bottom: 0.5rem;
-    gap: 0.25rem;
-    align-items: center;
-    font-size: 1.25rem;
-    line-height: 1.75rem;
-    font-weight: 700;
-    cursor: pointer;
-  }
-  > .content {
-    position: absolute;
-    z-index: 40;
-    padding: 0.25rem;
-    border-radius: 0.75rem;
-    background-color: ${({ theme }) => theme.colors.gray2};
-    color: ${({ theme }) => theme.colors.gray10};
-    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
-      0 2px 4px -1px rgba(0, 0, 0, 0.06);
-    > .item {
-      padding: 0.25rem;
-      padding-left: 0.5rem;
-      padding-right: 0.5rem;
-      border-radius: 0.75rem;
-      font-size: 0.875rem;
-      line-height: 1.25rem;
-      white-space: nowrap;
-      cursor: pointer;
-
-      :hover {
-        background-color: ${({ theme }) => theme.colors.gray4};
-      }
+    position: relative;
+    > .wrapper {
+        display: flex;
+        margin-top: 0.5rem;
+        margin-bottom: 0.5rem;
+        gap: 0.25rem;
+        align-items: center;
+        font-size: 1.25rem;
+        line-height: 1.75rem;
+        font-weight: 700;
+        cursor: pointer;
     }
-  }
-`
+    > .content {
+        position: absolute;
+        z-index: 40;
+        padding: 0.25rem;
+        border-radius: 0.75rem;
+        background-color: ${({ theme }) => theme.colors.gray2};
+        color: ${({ theme }) => theme.colors.gray10};
+        box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+            0 2px 4px -1px rgba(0, 0, 0, 0.06);
+        > .item {
+            padding: 0.25rem;
+            padding-left: 0.5rem;
+            padding-right: 0.5rem;
+            border-radius: 0.75rem;
+            font-size: 0.875rem;
+            line-height: 1.25rem;
+            white-space: nowrap;
+            cursor: pointer;
+
+            :hover {
+                background-color: ${({ theme }) => theme.colors.gray4};
+            }
+        }
+    }
+`;
