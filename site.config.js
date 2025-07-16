c0ac49d4886ba5e8d29b78824f6ec70fc90ef8f8
youngkeol <hyk1272@gmail.com>
2025-07-16 11:20:13 +0900

    이미지 변경, 포스트 카드 태그 겹침 수정

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
     // profile setting (required)
     profile: {
         name: "<Gori />",
-        image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+        image: "/avatar.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
         role: "",
         bio: "Hello 👋",
         email: "younggori.work@gmail.com",
--- src/routes/Feed/PostList/PostCard.tsx
+++ src/routes/Feed/PostList/PostCard.tsx
@@ -82,10 +82,11 @@ const StyledWrapper = styled(Link)`
                 0 4px 6px -2px rgba(0, 0, 0, 0.05);
         }
         > .category {
-            position: absolute;
-            top: 1rem;
-            left: 1rem;
-            z-index: 10;
+            --position: absolute;
+            --top: 1rem;
+            --left: 1rem;
+            --z-index: 10;
+            padding: 1rem 1rem 0 1rem;
         }
 
         > .thumbnail {
