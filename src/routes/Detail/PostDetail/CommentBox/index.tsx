7d3519171dade24dc916d2ecbb2bc730a62626c2
youngkeol <hyk1272@gmail.com>
2025-07-17 15:34:48 +0900

    [2025.07.17] 댓글 다크모드 전환 테스트

--- src/routes/Detail/PostDetail/CommentBox/index.tsx
+++ src/routes/Detail/PostDetail/CommentBox/index.tsx
@@ -2,6 +2,7 @@ import { TPost } from "src/types"
 import { CONFIG } from "site.config"
 import dynamic from "next/dynamic"
 import Giscus from "./Giscus";
+import useScheme from "src/hooks/useScheme";
 
 const UtterancesComponent = dynamic(
   () => {
@@ -21,13 +22,17 @@ type Props = {
 }
 
 const CommentBox: React.FC<Props> = ({ data }) => {
+  const scheme = useScheme();
+  // 값이 없으면 기본값 'light' 사용
+  const theme = scheme === "dark" ? "dark" : "light";
+ 
   return (
     <div>
       {CONFIG.utterances.enable && <UtterancesComponent issueTerm={data.id} />}
       {CONFIG.cusdis.enable && (
         <CusdisComponent id={data.id} slug={data.slug} title={data.title} />
       )}
-      <Giscus theme={scheme === "dark" ? "dark" : "light"} />
+      <Giscus theme={theme} />
     </div>
   )
 }
