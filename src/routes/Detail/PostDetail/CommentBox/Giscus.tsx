61836ab3fd692ef3331b4ded74f9cf7efe364249
youngkeol <hyk1272@gmail.com>
2025-07-17 15:20:01 +0900

    [2025.07.17] 댓글 기능 추가

--- src/routes/Detail/PostDetail/CommentBox/Giscus.tsx
+++ src/routes/Detail/PostDetail/CommentBox/Giscus.tsx
@@ -7,15 +7,15 @@ const Giscus = () => {
     if (giscusRef.current && !giscusRef.current.querySelector("iframe")) {
       const script = document.createElement("script");
       script.src = "https://giscus.app/client.js";
-      script.setAttribute("data-repo", "your/repo");
-      script.setAttribute("data-repo-id", "your-repo-id");
-      script.setAttribute("data-category", "Announcements");
-      script.setAttribute("data-category-id", "your-category-id");
+      script.setAttribute("data-repo", "youngkeol/notion_blog");
+      script.setAttribute("data-repo-id", "R_kgDOI1k-6w");
+      script.setAttribute("data-category", "Comments");
+      script.setAttribute("data-category-id", "DIC_kwDOI1k-684CtESe");
       script.setAttribute("data-mapping", "pathname");
       script.setAttribute("data-strict", "0");
       script.setAttribute("data-reactions-enabled", "1");
       script.setAttribute("data-emit-metadata", "0");
-      script.setAttribute("data-input-position", "bottom");
+      script.setAttribute("data-input-position", "top");
       script.setAttribute("data-theme", "light");
       script.setAttribute("data-lang", "ko");
       script.setAttribute("data-loading", "lazy");
